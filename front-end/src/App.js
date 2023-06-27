7f24f4d468a6b3c320a0f360f19241f045b3428a
dc4472 <dc4472@nyu.edu>
2023-06-26 23:29:33 -0400

    edit to App react page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+import CatFacts from './CatFacts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=props=> {
+
+      <div className="App">
+      <Router>
+          <main className="App-main">
+              <Routes>
+                <Route path="/" element={<CatFacts />} />
+
+              </Routes>
+          </main>
+      </Router>
     </div>
-  );
+
+
+
+
 }
 
 export default App;
