326093620e3615b8d6f23f59e7ecd61d3f90dc9f
dc4472 <dc4472@nyu.edu>
2023-06-26 20:17:12 -0400

    setup your server

--- back-end/server.js
+++ back-end/server.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const app=express()
+
+app.get("/", (req,res)=>{
+
+    res.json({"users": ["user1","user2","user3"]})
+
+})
+
+app.listen(5000, () =>{console.log("running on this port 5000")})
